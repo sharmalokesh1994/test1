1959182d1167c3788ef95ed1fe6fa027e34ff4b6
sharmalokesh1994 <43716471+sharmalokesh1994@users.noreply.github.com>
2020-08-29 14:42:57 +0530

    changes by github

--- test_proj/src/com/company/Main.java
+++ test_proj/src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hi all, this Lokesh");
+	System.out.println("Hi all, this Lokesh2");
     }
 }
