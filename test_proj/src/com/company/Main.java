d01701b4f206e083247d77284d0ecdc3cce691ca
sharmalokesh1994 <sharmalokesh1994@gmail.com>
2020-08-29 13:13:05 +0530

    SOUT2

--- test_proj/src/com/company/Main.java
+++ test_proj/src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hi all, this is Lokesh");
+        System.out.println("Hi all, this Lokesh");
     }
 }
